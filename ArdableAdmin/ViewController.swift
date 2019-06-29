0aabddce0be8f58df610e07cff815d4863df8e16
Yamamotonasu <deep.blue.deep@outlook.jp>
2019-06-29 15:30:42 +0900

    first commit

---
+++ ArdableAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArdableAdmin
+//
+//  Created by 山本裕太 on 2019/06/29.
+//  Copyright © 2019 山本裕太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
