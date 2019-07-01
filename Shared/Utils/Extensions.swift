c56a76e373f267ae0073dd5efc7d7f0433ccb85f
Yamamotonasu <deep.blue.deep@outlook.jp>
2019-07-01 23:40:06 +0900

    add alert when logging

--- Shared/Utils/Extensions.swift
+++ Shared/Utils/Extensions.swift
@@ -7,10 +7,20 @@
 //
 
 import Foundation
+import UIKit
 
 /// Stringプロトコルを拡張してisNotEmptyを実装
 extension String {
     var isNotEmpty: Bool {
         return !isEmpty
+    }
+}
+
+extension UIViewController {
+    func handleFireAuthError(error: Error) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
 }
