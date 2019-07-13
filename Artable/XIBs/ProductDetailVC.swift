7b5b1ec3b82077080eb4b6358df8177584073e5b
Yamamotonasu <deep.blue.deep@outlook.jp>
2019-07-13 10:52:49 +0900

    create product detail xib file

---
+++ Artable/XIBs/ProductDetailVC.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailVC.swift
+//  Artable
+//
+//  Created by 山本裕太 on 2019/07/13.
+//  Copyright © 2019 山本裕太. All rights reserved.
+//
+
+import UIKit
+
+class ProductDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
